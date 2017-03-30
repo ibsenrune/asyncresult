fa1092b4d2ac5faf46702aa729dcae0bca8d14a4
Rune Ibsen <rui@copenhagensoftware.com>
2017-03-30 21:19:23 +0200

    Implement AuthenticatedAsyncResult monad

--- Monads.fs
+++ Monads.fs
@@ -34,3 +34,78 @@ type AsyncResultBuilder() =
 [<AutoOpen>]
 module AsyncResultExpressionBuilder =
   let asyncResult = new AsyncResultBuilder()
+
+
+type AuthToken = AuthToken of string
+type ReaderAsyncResult<'TRead, 'T, 'TError> = Operation of ('TRead -> Async<Result<'T,'TError>>)
+
+module ReaderAsyncResult =
+  let map f (Operation action) =
+    let mapped auth =
+      action auth |> AsyncResult.map f
+    Operation mapped
+
+  let bind (f : 'a -> ReaderAsyncResult<'r,'b,'c>) (Operation action) =
+    let f' auth =
+      let f'' a =
+        let (Operation action') = f a
+        let res' = action' auth
+        res'
+      action auth |> AsyncResult.bind f''
+    Operation f'
+
+  let lift v =
+    Operation(fun _ -> AsyncResult.lift v)
+
+  let run (Operation action) auth =
+    action auth
+  
+type ReaderAsyncResultBuilder<'TRead>() =
+  member this.Return(v) = ReaderAsyncResult.lift v
+  member this.ReturnFrom(v) = v
+  member this.Bind(m, f) = ReaderAsyncResult.bind f m
+
+[<AutoOpen>]
+module AuthenticatedAsyncResultExpressionBuilder =
+  let authenticatedAsyncOperation = new ReaderAsyncResultBuilder<AuthToken>()
+
+module Usage =
+  type Subscription = { SubscriptionId : int }
+  let getPartnerSubscription : ReaderAsyncResult<AuthToken, Subscription, string> = 
+    (fun (auth : AuthToken) -> { SubscriptionId = 4 } |> AsyncResult.lift) |> Operation
+
+  let suspendSubscription (subscription : Subscription) =
+    (fun (auth : AuthToken) -> () |> AsyncResult.lift) |> Operation
+
+  let unsuspendSubscription (subscription : Subscription) =
+    (fun (auth : AuthToken) -> () |> AsyncResult.lift) |> Operation
+
+  let pause () =
+    (fun (auth : AuthToken) -> () |> AsyncResult.lift) |> Operation
+
+  let unpause () =
+    (fun (auth : AuthToken) -> () |> AsyncResult.lift) |> Operation
+
+  let sendEmail kind = authenticatedAsyncOperation {
+    return ()
+  }
+
+  let changeEmail email = authenticatedAsyncOperation {
+    return ()
+  }
+
+  let updateFullname fullName = authenticatedAsyncOperation {
+    return ()
+  }
+
+  let suspendUser = authenticatedAsyncOperation { 
+    let! s = getPartnerSubscription
+    do! suspendSubscription s
+    do! unpause ()
+  }
+
+  let unSuspendUser = authenticatedAsyncOperation {
+    do! pause ()
+    let! subscription = getPartnerSubscription
+    do! unsuspendSubscription subscription
+  }
